e556ea65785bfcfd243cf325e6adcb7f8ea65dce
yuya sato <ysato.5654@gmail.com>
2018-01-14 14:52:00 +0800

    add ‘Supporter’ (empty) as API classification and ‘SupporterUser’ (empty) as Twicas API object

---
+++ lib/twicas_stream/supporter.rb
@@ -0,0 +1,24 @@
+#! /opt/local/bin/ruby
+# coding: utf-8
+
+require File.expand_path(File.dirname(__FILE__) + '/twicas_api_object/user')
+require File.expand_path(File.dirname(__FILE__) + '/twicas_api_object/supporter_user')
+
+module TwicasStream
+	module Supporter
+		class GetSupportingStatus
+		end
+
+		class SupportUser
+		end
+
+		class UnsupportUser
+		end
+
+		class SupportingList
+		end
+
+		class SupporterList
+		end
+	end
+end
---
+++ lib/twicas_stream/twicas_api_object/supporter_user.rb
@@ -0,0 +1,9 @@
+#! /opt/local/bin/ruby
+# coding: utf-8
+
+module TwicasStream
+	module TwicasApiObject
+		class SupporterUser
+		end
+	end
+end
